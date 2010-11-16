01058856645283ad79ce2fd0caf8a81475e225c9
Diego Guimaraes <diegodukao@gmail.com>
2010-11-16 11:25:04 -0200

    Editing the menu: item 'tools' added

--- pyge.py
+++ pyge.py
@@ -26,6 +26,22 @@ class PyGE:
         
     def on_window_destroy(self, widget, data=None):
         gtk.main_quit()
+        
+    # We call error_message() any time we want to display an error message to 
+    # the user. It will both show an error dialog and log the error to the 
+    # terminal window.
+    def error_message(self, message):
+    
+        # log to terminal window
+        print message
+        
+        # create an error message dialog and display modally to the user
+        dialog = gtk.MessageDialog(None,
+                                   gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                                   gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, message)
+        
+        dialog.run()
+        dialog.destroy()
 
 if __name__ == "__main__":
     pyge = PyGE()
