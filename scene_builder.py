2acefa8b0e1655d7dc6f7c697896b3e5d62ff760
Diego Guimaraes <diegodukao@gmail.com>
2010-12-02 00:21:07 -0200

    creating the bg

--- scene_builder.py
+++ scene_builder.py
@@ -9,15 +9,28 @@ class SceneBuilder:
         self.scene_xml = xml.dom.minidom.parse(xml_filename)
         
     def create_screen(self):
-        width, height = self.get_screen_size()
+        width, height = self.get_screen_size_from_xml()
         screen = pygame.display.set_mode((int(width), int(height)))
         
         return screen
         
-    def get_screen_size(self):
+    def get_screen_size_from_xml(self):
         scene_node = self.scene_xml.getElementsByTagName("scene")
         screen_width = scene_node[0].getAttribute("width")
         screen_height = scene_node[0].getAttribute("height")
         
         return screen_width, screen_height
+        
+    def create_bg(self):
+        bg_path = self.get_bg_path_from_xml()
+        bg = pygame.image.load(bg_path)
+        
+        return bg, bg.get_rect
+        
+        
+    def get_bg_path_from_xml(self):
+        bg_node = self.scene_xml.getElementsByTagName("background")
+        bg_path = bg_node[0].childNodes[0].nodeValue
+        
+        return bg_path
         
--- test_scene_builder.py
+++ test_scene_builder.py
@@ -4,5 +4,9 @@ from scene_builder import SceneBuilder
 
 builder = SceneBuilder()
 
-screen = builder.create_screen()
+#screen = builder.create_screen()
+
+bg, bg_rect = builder.create_bg()
+print bg
+print bg_rect
 
