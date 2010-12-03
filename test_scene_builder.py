728f17b3e918d66cd69a2227494d9c324bca85aa
Diego Guimaraes <diegodukao@gmail.com>
2010-12-03 12:54:15 -0200

    commenting the test_scene_builder.py file

--- test_scene_builder.py
+++ test_scene_builder.py
@@ -6,24 +6,24 @@ from scene_builder import SceneBuilder
 
 pygame.init()
 
+# Creating the scene with the Scene Builder
 builder = SceneBuilder()
-
 screen = builder.create_screen()
-
 bg, bg_rect = builder.create_bg()
-
 sprites = builder.get_sprites()
 
+# Main loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             
+    # Drawing the background
     screen.blit(bg, bg_rect)
     
+    # Drawing the sprites
     for sprite in sprites.itervalues():
          screen.blit(sprite.image, sprite.rect)
     
+    # Updating the screen
     pygame.display.flip()
-
-
